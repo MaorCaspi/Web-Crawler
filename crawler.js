ba1c1378964d84dd3fa8a5259ad0b50d92cb4e43
maorcaspi <maorcaspi1996@gmail.com>
2024-04-28 12:00:37 +0300

    Add Check if url is already in the database

--- crawler.js
+++ crawler.js
@@ -11,27 +11,36 @@ const crawler = new Crawler({
       } else {
           const $ = res.$;
           const url = res.options.uri.toLowerCase();
-          const html = $('html');
-          
-          // Save the crawled URL to the database
-          try {
-                await Url.create({
-                    url: url,
-                    htmlContent: html
-                });
-            console.log(`URL ${url} saved to the database.`);
-
+         try {
+            const exists = await Url.findOne({'url' : url});
+            if (exists == null) {
+                const html = $('html');
+                // Save the crawled URL to the database
+                try {
+                    await Url.create({
+                        url: url,
+                        htmlContent: html
+                    });
+                    console.log(`URL ${url} saved to the database.`);
+                }
+                catch (err) {
+                    console.error(`Error saving URL ${url} to the database: ${err.message}`);
+                }
             }
-            catch (err) {
-                console.error(`Error saving URL ${url} to the database: ${err.message}`);
+            else {
+                console.log(`URL ${url} is already in the database.`);
             }
+        }
+        catch (err) {
+            console.error(`Error with check if the URL ${url} is in the database: ${err.message}`);
+        }
+        
+        const links = $("a")
+        .map((i, link) => link.attribs.href)
+        .get()
+        .filter(link => link.startsWith("http"));
 
-          const links = $("a")
-          .map((i, link) => link.attribs.href)
-          .get()
-          .filter(link => link.startsWith("http"));
-
-          crawler.queue(links);
+        crawler.queue(links);
 
       }
       done();
